f9eca54c0ebf49743774d28fc1ae78bf47748134
mikeagamcolum <55404593+mikeagamcolum@users.noreply.github.com>
2020-05-06 20:29:15 -0500

    final exam cpp2

---
+++ src/main.cpp
@@ -0,0 +1,5 @@
+#include "funcs.h"
+
+int main() {
+  return 0; // nothing to do here
+}
---
+++ test/tests.cpp
@@ -0,0 +1,15 @@
+#include <gtest/gtest.h>
+#include "../src/funcs.h"
+
+TEST(rankVersus, PerfectHandEqualsOne) {
+    ASSERT_EQ(Card(8, HEARTS).rankVersus({Card(JACK, HEARTS), Card(JACK, DIAMONDS), Card(ACE, HEARTS), Card(KING, HEARTS), Card(QUEEN, HEARTS)}), 1);
+}
+
+TEST(rankVersus, JunkEqualsZero) {
+    ASSERT_EQ(Card(8, CLUBS).rankVersus({Card(JACK, HEARTS), Card(JACK, DIAMONDS), Card(ACE, HEARTS), Card(KING, HEARTS), Card(QUEEN, HEARTS)}), 0);
+}
+
+int main(int argc, char** argv){
+    testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
